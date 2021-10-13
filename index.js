1dd268f0bfcfd6187898a8fbdbf635b3cc0e4d08
Julia Loeschel <juloeschel@gmail.com>
2021-10-13 09:23:15 +0200

    Positen bookmark and add js for index and create

---
+++ index.js
@@ -0,0 +1,30 @@
+const toggleAnswerButton = document.querySelector(".js-btn-answer");
+const showAnswer = document.querySelector(".js-btn-showAnswer");
+const hideAnswer = document.querySelector(".js-btn-hideAnswer");
+const showAnswerH = document.querySelector(".js-answer-heading ");
+const hideAnswerT = document.querySelector(".js-answer-text");
+
+toggleAnswerButton.addEventListener("click", () => {
+  showAnswer.classList.toggle("hidden");
+  hideAnswer.classList.toggle("hidden");
+  showAnswerH.classList.toggle("hidden");
+  hideAnswerT.classList.toggle("hidden");
+});
+
+const bookmark = document.querySelector(".js-bookmark");
+
+bookmark.addEventListener("click", () => {
+  bookmark.classList.toggle("card-set__bookmarklabel--booked");
+});
+
+const homebtn = document.querySelector(".js-home-btn");
+const bookmarkbtn = document.querySelector(".js-bookmark-btn");
+
+homebtn.addEventListener("click", () => {
+  homebtn.classList.add("nav__active");
+  bookmarkbtn.classList.remove("nav__active");
+});
+bookmarkbtn.addEventListener("click", () => {
+  bookmarkbtn.classList.add("nav__active");
+  homebtn.classList.remove("nav__active");
+});
