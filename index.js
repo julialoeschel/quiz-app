42be2de3e769fe3e7ac7ce7b91b72a2f67387ee3
Julia Loeschel <juloeschel@gmail.com>
2021-10-15 17:20:54 +0200

    First attempf - not working jet

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
-const toggleAnswerButton = document.querySelector(".js-btn-answer");
-const showAnswer = document.querySelector(".js-btn-showAnswer");
-const hideAnswer = document.querySelector(".js-btn-hideAnswer");
-const showAnswerH = document.querySelector(".js-answer-heading ");
-const hideAnswerT = document.querySelector(".js-answer-text");
-const bookmark = document.querySelector(".js-bookmark");
-const homebtn = document.querySelector(".js-home-btn");
-const bookmarkbtn = document.querySelector(".js-bookmark-btn");
+const cardSet = document.querySelectorAll("[data-js=card-set]");
+cardSet.forEach((card) => {
+  const bookmark = card.querySelector("[data-js=bookmark]");
+  bookmark.addEventListener("click", () => {
+    bookmark.classList.toggle("card-set__bookmarklabel--booked");
+  });
 
-toggleAnswerButton.addEventListener("click", () => {
-  showAnswer.classList.toggle("hidden");
-  hideAnswer.classList.toggle("hidden");
-  showAnswerH.classList.toggle("hidden");
-  hideAnswerT.classList.toggle("hidden");
-});
+  const btnAnswer = card.querySelector("[data-js=btn-answer]");
 
-bookmark.addEventListener("click", () => {
-  bookmark.classList.toggle("card-set__bookmarklabel--booked");
-});
-
-homebtn.addEventListener("click", () => {
-  homebtn.classList.add("nav__active");
-  bookmarkbtn.classList.remove("nav__active");
-});
-bookmarkbtn.addEventListener("click", () => {
-  bookmarkbtn.classList.add("nav__active");
-  homebtn.classList.remove("nav__active");
+  /*const btnShow = card.querySelector("[data-js=btn-show]");
+  const btnHide = card.querySelector("[data-js=btn-hide]");*/
+  btnAnswer.addEventListener("click", () => {
+    console.log("hi");
+    /* btnShow.classList.toggle("hidden");
+    btnHide.classList.toggle("hidden");*/
+  });
 });
