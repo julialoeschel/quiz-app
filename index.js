d31c559827068a49fee4b88f934a92d44a2e563a
Julia Loeschel <juloeschel@gmail.com>
2021-10-16 14:17:46 +0200

    make nav nice

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const cardSet = document.querySelectorAll("[data-js=card-set]");
+
 cardSet.forEach((card) => {
   const bookmark = card.querySelector("[data-js=bookmark]");
+  const AnswerBtn = card.querySelector("[data-js=btn-answer]");
+  const showAnswer = card.querySelector("[data-js=showAnswer]");
+  const hideAnswer = card.querySelector("[data-js=hideAnswer]");
+  const answerH = card.querySelector("[data-js=Answer-heading]");
+  const answerT = card.querySelector("[data-js=answer-text]");
+
   bookmark.addEventListener("click", () => {
     bookmark.classList.toggle("card-set__bookmarklabel--booked");
   });
 
-  const btnAnswer = card.querySelector("[data-js=btn-answer]");
-
-  /*const btnShow = card.querySelector("[data-js=btn-show]");
-  const btnHide = card.querySelector("[data-js=btn-hide]");*/
-  btnAnswer.addEventListener("click", () => {
-    console.log("hi");
-    /* btnShow.classList.toggle("hidden");
-    btnHide.classList.toggle("hidden");*/
+  AnswerBtn.addEventListener("click", () => {
+    showAnswer.classList.toggle("hidden");
+    hideAnswer.classList.toggle("hidden");
+    answerH.classList.toggle("hidden");
+    answerT.classList.toggle("hidden");
   });
 });
+
+const homeBtn = document.querySelector("[data-js=home-button]");
