3aa20c843b6f5b0e467a0d842ab5110fff67d270
Julia Loeschel <juloeschel@gmail.com>
2021-10-16 16:18:48 +0200

    Updtae the charecter calculation at create page

--- index.js
+++ index.js
@@ -77,3 +77,24 @@ profileBtn.addEventListener("click", () => {
   createPage.classList.add("hidden");
   profilePage.classList.remove("hidden");
 });
+
+const createAddQuestion = document.querySelector("[data-js=text-addquestion]");
+const amountLeftAddQuestion = document.querySelector(
+  "[data-js=create-amount-add-question]"
+);
+const createAddAnswer = document.querySelector("[data-js=text-addanswer]");
+const amountLeftAddAnswer = document.querySelector(
+  "[data-js=create-amount-add-answer]"
+);
+
+createAddQuestion.addEventListener("input", () => {
+  const questionTextLength = createAddQuestion.value.length;
+  const maxLengthAddQuestion = createAddQuestion.maxLength;
+  amountLeftAddQuestion.innerText = maxLengthAddQuestion - questionTextLength;
+  console.log(maxLengthAddQuestion);
+});
+
+createAddAnswer.addEventListener("input", () => {
+  amountLeftAddAnswer.innerText =
+    createAddAnswer.maxLength - createAddAnswer.value.length;
+});
